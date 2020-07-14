8e27be17aba6a36b311a9b282da47453ea5d0c02
Anwesh43 <anweshthecool0@gmail.com>
2020-07-15 03:48:58 +0530

    adding OrbitalCircleView class

--- orbitalcircleview/src/main/java/com/anwesh/uiprojects/orbitalcircleview/OrbitalCircleView.kt
+++ orbitalcircleview/src/main/java/com/anwesh/uiprojects/orbitalcircleview/OrbitalCircleView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawOCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawOrbitalCircles(scale, w, h, paint)
 }
+
+class OrbitalCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
