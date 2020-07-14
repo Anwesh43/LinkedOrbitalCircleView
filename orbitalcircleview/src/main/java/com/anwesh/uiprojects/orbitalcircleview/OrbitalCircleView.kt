e2f34a9548f35aab97bdfb127eca7b1ec447a923
Anwesh43 <anweshthecool0@gmail.com>
2020-07-15 04:00:04 +0530

    adding OCNode class

--- orbitalcircleview/src/main/java/com/anwesh/uiprojects/orbitalcircleview/OrbitalCircleView.kt
+++ orbitalcircleview/src/main/java/com/anwesh/uiprojects/orbitalcircleview/OrbitalCircleView.kt
@@ -127,4 +127,45 @@ class OrbitalCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class OCNode(var i : Int, val state : State = State()) {
+
+        private var next : OCNode? = null
+        private var prev : OCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size  - 1) {
+                next = OCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawOCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : OCNode {
+            var curr : OCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
